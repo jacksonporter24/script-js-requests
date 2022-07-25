a7d196f2b8f35e93e8c5438fd7e348d5d74874b2
Jackson Porter <readjacksonporter@gmail.com>
2022-07-25 17:21:28 -0600

    day one

--- client/script.js
+++ client/script.js
@@ -15,22 +15,34 @@
 
 
 
-// CODE HERE
-const sayHelloButton = document.querySelector('#say-hello-button')
 
+// CODE HERE
+const exampleButton = document.querySelector('#example-button')
+const helloText = document.querySelector('#hello-text')
+const storyCircle = document.querySelector('#circle')
+helloText.style.display = "none";
+storyCircle.style.display = "none";
+
+// exampleButton.onclick = function(
+exampleButton.addEventListener("click",() =>{
+    exampleButton.style.backgroundColor = "black";
+    exampleButton.style.color = "white";
+    helloText.style.display = "block";
+    storyCircle.style.display = "block";
+    
+})
 
 // PROBLEM 2
 /*
     Create a function that changes sayHelloButton's background color to black and its text color to white (you can use the .style object or create a CSS class and use classList.add)
-    
     Attach a mouseover event to sayHelloButton that calls the function you wrote
 */
 
 // CODE HERE
-sayHelloButton.addEventListener("mouseover", () => {
-    sayHelloButton.style.backgroundColor = "black";
-    sayHelloButton.style.color = "white";
-})
+// exampleButton.addEventListener("mouseover", () => {
+//     exampleButton.style.backgroundColor = "black";
+//     exampleButton.style.color = "white";
+// })
 
 
 
@@ -38,22 +50,19 @@ sayHelloButton.addEventListener("mouseover", () => {
 // PROBLEM 3
 /*
     Now you can see that the button colors change, but they do not change back when we take the mouse off of the button.
-
-    Write another function that changes the button back to its original colors. #EFEFEF for the background and black for the text.
-
+    Write another function that changes the button back to its original colors. #EFEFEF for the background and black for the text
     Attach another listener that fires your second function when the mouseout event occurs on the button
 */
 
 // CODE HERE
-sayHelloButton.addEventListener("mouseout", () => {
-    sayHelloButton.style.backgroundColor = "#EFEFEF";
-    sayHelloButton.style.color = "black";
+exampleButton.addEventListener("mouseout", () => {
+    exampleButton.style.backgroundColor = "#EFEFEF";
+    exampleButton.style.color = "black";
 })
 
 // PROBLEM 4
 /*
     Now lets see if we can make a request to our server when we click the button
-
     Add a 3rd event listener to sayHelloButton and trigger the sayHello function when the button is clicked
 */
 
@@ -69,16 +78,13 @@ const sayHello = () => {
 // DO NOT EDIT FUNCTION
 
 // CODE HERE
-sayHelloButton.addEventListener('click', sayHello)
+exampleButton.addEventListener('click', sayHello)
 
 // PROBLEM 5 
 /*
     Now that we have attached a few event listeners why dont we try adding a request? 
-    
     Below you will find an event listener on a button. 
-    
     Use axios inside the ohMy function to make a GET request to 'http://localhost:3000/animals' 
-    
     Handle the promise that's returned with a .then, which you should pass a callback function to. Inside the callback function, console.log the response's data (in the intermediate instructions we'll come back to this function and add HTML).
 */ 
 
@@ -96,13 +102,9 @@ document.getElementById('animals-button').addEventListener('click', ohMy)
 // PROBLEM 6 
 /*
     Now lets see if you can send a request param! inside repeatMyParam function below  make get request to 'http://localhost:3000/repeat/{SOMEPARAM}', but with a string instead of {SOMEPARAM}.  
-
     The function that runs when this request is made will return whatever parameter you sent 
-
     Handle the promise returned from the request with a .then, which will take in a callback -- the callback function should print the response.data.
-    
     Outside of the function, select the button with the id "repeat-button" and add a click event listener that calls the repeatMyParam function.
-    
     We'll be updating this function in the next problem.
 */
 
@@ -126,7 +128,6 @@ document.getElementById('repeat-button').addEventListener('click', repeatMyParam
 // PROBLEM 7
 /*
     Now that we have the response data, let's add it to our web page! 
-    
     Inside the repeatMyParam function above, grab the element with the id of 'repeat-text' and set its textContent property equal to the response data.
 */
 
@@ -134,9 +135,7 @@ document.getElementById('repeat-button').addEventListener('click', repeatMyParam
 // PROBLEM 8
 /*
     Time to attach a query to our request!
-
     Write a function that makes a get request to 'http://localhost:3000/query-test', with a query of your choice on the end!
-
     Outside of your new function, select the button with the id "query-button" and add a click event listener that calls your function.
 */
 
@@ -161,7 +160,6 @@ const queryButton = document.getElementById('query-button').addEventListener('cl
 // PROBLEM 9
 /* 
     Back in the ohMy function on Problem 5, replace the console log in the promise's callback with a for loop that loops over res.data. 
-
     On each iteration of the loop, create a new p element. Set its textContent equal the string at the current index (i) and then append the new p element onto the document's body. 
 */
 
@@ -170,16 +168,11 @@ const queryButton = document.getElementById('query-button').addEventListener('cl
 // PROBLEM 10 
 /*
     In the function that you wrote for Problem 8, change the URL to test a couple different scenarios. 
-
     1: Send no queries on the URL -- what happened? 
-
     2: Send more than 1 query on the URL -- what happened? 
 */
 
 // Edit code in Problem 8
-
-
-
 ////////////
 //ADVANCED//
 ////////////
